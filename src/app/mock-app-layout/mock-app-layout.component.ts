157d84e461c0bc8f11bb8bf4422c8960bcced729
John R <pawn002@hotmail.com>
2023-05-12 11:56:04 -0400

    feat: added lorem generator

--- src/app/mock-app-layout/mock-app-layout.component.ts
+++ src/app/mock-app-layout/mock-app-layout.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+import { LoremIpsum } from 'lorem-ipsum';
+
 @Component({
   selector: 'app-mock-app-layout',
   templateUrl: './mock-app-layout.component.html',
-  styleUrls: ['./mock-app-layout.component.scss']
+  styleUrls: ['./mock-app-layout.component.scss'],
 })
 export class MockAppLayoutComponent {
+  baseIpsum: LoremIpsum;
+  anIpsum;
+  anotherIpsum;
 
+  constructor() {
+    this.baseIpsum = new LoremIpsum({
+      sentencesPerParagraph: {
+        max: 8,
+        min: 4,
+      },
+      wordsPerSentence: {
+        max: 16,
+        min: 4,
+      },
+    });
+
+    this.anIpsum = this.baseIpsum.generateParagraphs(1);
+
+    this.anotherIpsum = this.baseIpsum.generateParagraphs(1);
+  }
 }
